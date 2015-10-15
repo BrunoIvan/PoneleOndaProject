0d6d5baca3941b2454167beb64796711b5e6c8bb
BrunoIvan <bruno_cabj_bersuit@hotmail.com>
2015-10-14 23:21:20 -0300

    Atributos nulos para Usuarios y ausencia de unicode

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
-#prueba para subir
+from django.contrib.auth.models import User
+
 class Establecimiento(models.Model):
 	nombre		=	models.CharField(max_length = 200)
 	direccion	=	models.CharField(max_length = 200)
@@ -8,32 +9,41 @@ class Establecimiento(models.Model):
 	latitud		= 	models.FloatField()
 	longitud	= 	models.FloatField()
 	rubro		=	models.ForeignKey("Rubro")
-	def __unicode__(self):return self.nombre
+	
+	def __unicode__(self):
+		return self.nombre
 
 class Ciudad(models.Model):
 	nombre			=	models.CharField(max_length = 200)
 	codigo_postal	=	models.CharField(max_length = 8)
 	provincia		= 	models.ForeignKey("Provincia")
-	def __unicode__(self):return self.nombre
+	
+	def __unicode__(self):
+		return self.nombre
 
 class Provincia(models.Model):
 	nombre	 = 	models.CharField(max_length = 50)
-	def __unicode__(self):return self.nombre
+	
+	def __unicode__(self):
+		return self.nombre
 
 class Rubro(models.Model):
 	nombre	 = 	models.CharField(max_length = 200)
-	def __unicode__(self):return self.nombre
+	
+	def __unicode__(self):
+		return self.nombre
 
 class Calificacion(models.Model):
 	puntaje	 		= 	models.IntegerField()
 	comentario 		=	models.CharField(max_length = 200)
 	establecimiento = 	models.ForeignKey("Establecimiento")
 	usuario			=	models.ForeignKey("Usuario")
-	def __unicode__(self):return self.puntaje
+	
+	def __unicode__(self):
+		return self.puntaje
 
 class Usuario(models.Model):
-	fb_id		=	models.CharField(max_length = 200)
-	tw_id		=	models.CharField(max_length = 200)
-	google_id	=	models.CharField(max_length = 200)
-	def __unicode__(self):return self.id
+	fb_id		=	models.CharField(max_length = 200, null = True)
+	tw_id		=	models.CharField(max_length = 200, null = True)
+	google_id	=	models.CharField(max_length = 200, null = True)
 	
